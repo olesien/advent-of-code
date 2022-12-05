bea0b9f94bd6e46c75ca22b9d1245d3b11b9ac06
Linus <olesien2@gmail.com>
2022-12-05 12:02:57 +0100

    Did part 2

--- day-5.ts
+++ day-5.ts
@@ -3,6 +3,7 @@ const run = async () => {
     const rows = crates.split("\n");
     let cratesObject: { [key: string]: string[] } = {};
     let instructions: { from: number; to: number }[] = [];
+    let instructions2: { from: number; to: number; count: number }[] = [];
     let cratesDone = false;
 
     rows.forEach((row) => {
@@ -29,22 +30,25 @@ const run = async () => {
                 for (let index = 0; index < count; index++) {
                     instructions.push({ from, to });
                 }
+
+                //part 2
+                instructions2.push({ from, to, count });
                 //console.log(splitString);
             }
         }
     });
 
+    const cratesObject2: { [key: string]: string[] } = { ...cratesObject };
     console.log(cratesObject);
-    console.log(instructions);
 
-    instructions.forEach((instruction) => {
-        const crateFrom = cratesObject[instruction.from];
-        const crateTo = cratesObject[instruction.to];
-        const container = crateFrom.shift();
-        if (container) {
-            crateTo.unshift(container);
-        }
-    });
+    // instructions.forEach((instruction) => {
+    //     const crateFrom = cratesObject[instruction.from];
+    //     const crateTo = cratesObject[instruction.to];
+    //     const container = crateFrom.shift();
+    //     if (container) {
+    //         crateTo.unshift(container);
+    //     }
+    // });
 
     console.log(cratesObject);
     const letterString = Object.values(cratesObject).reduce(
@@ -54,5 +58,36 @@ const run = async () => {
         ""
     );
     console.log(letterString);
+    console.log("PART 2 XXXXXXXXXXXXXXX");
+    //Part 2
+    instructions2.forEach((instruction) => {
+        console.log(instruction);
+        let crateFrom = cratesObject2[instruction.from];
+        let crateTo = cratesObject2[instruction.to];
+        console.log(cratesObject2);
+        console.log("----");
+        console.log(crateFrom);
+        let containers = crateFrom.filter(
+            (container, index) => index >= 0 && index <= instruction.count - 1
+        );
+        console.log(instruction.count);
+        console.log(containers);
+        crateFrom.splice(0, instruction.count);
+        if (containers) {
+            //crateTo.unshift(...containers);
+            crateTo.unshift(...containers);
+            //crateTo.splice(0, 0, ...containers);
+        }
+        console.log(cratesObject2);
+    });
+
+    const letterString2 = Object.values(cratesObject2).reduce(
+        (priorLetters, letterArray) => {
+            return priorLetters + letterArray[0];
+        },
+        ""
+    );
+    console.log(cratesObject2);
+    console.log(letterString2);
 };
 run();
