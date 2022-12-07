86363cd828158eae45c1d90093ab14cd05119b99
olesien <olesien2@gmail.com>
2022-12-07 22:10:43 +0100

    help

---
+++ day-7.ts
@@ -0,0 +1,129 @@
+const run = async () => {
+    //2061777 <- right answer
+    const filesystem = await Deno.readTextFile("./filesystem.txt");
+    //console.log(filesystem);
+    const lines = filesystem.split("\n");
+    //console.log(lines);
+    const {
+        directories,
+        currentPath,
+    }: { directories: { [key: string]: string }; currentPath: string } =
+        lines.reduce(
+            (system, line) => {
+                const splitPath = system.currentPath.split("/");
+                // console.log(line);
+                // console.log(system);
+                // console.log(splitPath);
+                if (line[0] === "$") {
+                    //Is command
+                    if (line[2] === "l" && line[3] === "s") {
+                        return { ...system, ls: line.split(" ")[2] };
+                    }
+                    if (line[2] === "c" && line[3] === "d") {
+                        //cd
+                        if (line.includes("..")) {
+                            //Go back
+                            if (system.currentPath !== "/") {
+                                //Go back once
+                                splitPath.pop();
+                                return {
+                                    ...system,
+                                    currentPath: splitPath.join("/"),
+                                };
+                            } else {
+                                return {
+                                    ...system,
+                                    currentPath: "/",
+                                };
+                            }
+                        } else {
+                            //Add to path
+                            const actionArray = line.split(" ");
+                            const newDirectory = actionArray[2];
+                            if (newDirectory === "/") {
+                                return { ...system, currentPath: "/" };
+                            }
+                            if (system.currentPath === "/") {
+                                return {
+                                    ...system,
+                                    currentPath: "/" + newDirectory,
+                                };
+                            }
+                            splitPath.push(newDirectory);
+
+                            return {
+                                ...system,
+                                currentPath: splitPath.join("/"),
+                            };
+                        }
+                    }
+                } else if ("ls" in system) {
+                    //Read line as ls
+                    //console.log("Under " + system.currentPath);
+                    const [size, file] = line.split(" ");
+                    //console.log(size, file);
+                    if (size === "dir") {
+                        return system;
+                    }
+                    const exactFile =
+                        system.currentPath +
+                        (system.currentPath !== "/" ? "/" : "") +
+                        file;
+                    if (exactFile in system.directories)
+                        return {
+                            ...system,
+                            directories: {
+                                ...system.directories,
+                                [exactFile]:
+                                    Number(system.directories[exactFile]) +
+                                    Number(size),
+                            },
+                        };
+                    return {
+                        ...system,
+                        directories: {
+                            ...system.directories,
+                            [exactFile]: Number(size),
+                        },
+                    };
+                }
+                return system;
+            },
+            { directories: {}, currentPath: "/", ls: "" }
+        );
+    console.log(directories);
+
+    //Fix so they all still have UNIQUE paths!
+    const sizes: { [key: string]: number } = Object.keys(directories).reduce(
+        (sizes, path) => {
+            const newSizes: { [key: string]: number } = { ...sizes };
+            const size = directories[path];
+            const splitPath = path.split("/");
+            const file = splitPath[splitPath.length - 1];
+            splitPath.forEach((directory, index) => {
+                const path = directory !== "" ? directory : "/";
+                if (path !== file) {
+                    if (path in newSizes) {
+                        newSizes[path] = newSizes[path] + Number(size);
+                    } else {
+                        newSizes[path] = Number(size);
+                    }
+                }
+            });
+            return newSizes;
+        },
+        {}
+    );
+    console.log(sizes);
+
+    //This is the answer I want to get at
+    const summedSizes = Object.keys(sizes).reduce((totalSize, directory) => {
+        const size = sizes[directory];
+        if (size <= 100000) {
+            return totalSize + size;
+        }
+        return totalSize;
+    }, 0);
+    console.log(summedSizes);
+};
+run();
