365d9c149f4b3d99c8778b48869c1bf600180911
olesien <olesien2@gmail.com>
2022-12-11 20:27:01 +0100

    Added so it works correctly but inspect is wrong..

--- day-11.ts
+++ day-11.ts
@@ -3,7 +3,7 @@ const run = async () => {
     const monkeyList = monkeys.split("\n\n");
     console.log(monkeyList);
 
-    const monekyItems = monkeyList.reduce((prevItems: number[][], monkey) => {
+    const monkeyItems = monkeyList.reduce((prevItems: number[][], monkey) => {
         const [monkeyMisc, startingItems] = monkey.split("\n");
         const items = startingItems.split(/[:,]+/).map((item) => Number(item));
         prevItems.push(items);
@@ -11,86 +11,103 @@ const run = async () => {
     }, []);
     const inspectedItems: number[][] = monkeyList.map(() => []);
     //Add for loop here up to 20 for each round!
-    monkeyList.forEach((monkey, monkeyIndex) => {
-        const [
-            monkeyMisc,
-            startingItems,
-            operation,
-            test,
-            testTrue,
-            testFalse,
-        ] = monkey.split("\n");
-        console.log(
-            monkeyMisc,
-            startingItems,
-            operation,
-            test,
-            testTrue,
-            testFalse
-        );
-        const items = [...monekyItems[monkeyIndex]];
-        items.shift();
-        console.log(items);
 
-        let worryLevel = 0;
-        const operationStats = operation.split("=")[1];
-        //console.log(operationStats);
-        let changeNumber: string | number = operationStats.split(/[*+]+/)[1];
-        const divisibleBy = Number(test.split("by")[1]);
-
-        //Operation: First bit is ALWAYS "old", second can be "old" or number, with it either being * or + as seperator
-        const increaseWorryLevel = () => {
-            if (String(changeNumber).includes("old")) {
-                changeNumber = worryLevel;
-            } else {
-                changeNumber = Number(changeNumber);
-            }
-            if (operationStats.includes("*")) {
-                //Mulitply
-                return worryLevel * changeNumber;
-            } else {
-                //Plus
-                return worryLevel + changeNumber;
-            }
-        };
-        console.log("Monkey: " + monkeyIndex + ":");
-        items.forEach((item, index) => {
+    for (let round = 1; round <= 20; round++) {
+        console.log("round " + round);
+        monkeyList.forEach((monkey, monkeyIndex) => {
+            const [
+                monkeyMisc,
+                startingItems,
+                operation,
+                test,
+                testTrue,
+                testFalse,
+            ] = monkey.split("\n");
             console.log(
-                " Monkey inspects an item with a worry level of " + item
+                monkeyMisc,
+                startingItems,
+                operation,
+                test,
+                testTrue,
+                testFalse
             );
-            inspectedItems[monkeyIndex].push(item);
-            worryLevel = item;
-            worryLevel = increaseWorryLevel();
-            console.log("  Worry level is changed to " + worryLevel);
-            worryLevel = Math.floor(worryLevel / 3);
-            console.log(
-                "  Monkey gets bored with item. Worry level is divided by 3 to " +
-                    worryLevel
-            );
-            //console.log(item);
-            let newIndex = 0;
-            if (worryLevel % divisibleBy === 0) {
-                //Throw to first
-                //console.log("first");
-                newIndex = Number(testTrue.split("monkey")[1]);
-            } else {
-                //Throw to second
-                //console.log("second");
-                newIndex = Number(testFalse.split("monkey")[1]);
+            const items = [...monkeyItems[monkeyIndex]];
+            if (round === 1) {
+                items.shift();
             }
 
-            monekyItems[newIndex].push(worryLevel);
+            console.log(items);
 
-            console.log(
-                "  Item with worry level " +
-                    worryLevel +
-                    " is thrown to monkey " +
-                    newIndex
-            );
+            let worryLevel = 0;
+            const operationStats = operation.split("=")[1];
+            //console.log(operationStats);
+            const changeNumberLegacy: string | number =
+                operationStats.split(/[*+]+/)[1];
+            let changeNumber: string | number =
+                operationStats.split(/[*+]+/)[1];
+            const divisibleBy = Number(test.split("by")[1]);
+
+            //Operation: First bit is ALWAYS "old", second can be "old" or number, with it either being * or + as seperator
+            const increaseWorryLevel = (item: number) => {
+                if (String(changeNumberLegacy).includes("old")) {
+                    changeNumber = item;
+                } else {
+                    changeNumber = Number(changeNumber);
+                }
+                if (operationStats.includes("*")) {
+                    //Mulitply
+                    return worryLevel * changeNumber;
+                } else {
+                    //Plus
+                    return worryLevel + changeNumber;
+                }
+            };
+            console.log("Monkey: " + monkeyIndex + ":");
+            inspectedItems[monkeyIndex].push(0);
+            items.forEach((item, index) => {
+                console.log(
+                    " Monkey inspects an item with a worry level of " + item
+                );
+
+                worryLevel = item;
+                worryLevel = increaseWorryLevel(item);
+                console.log("  Worry level is changed to " + worryLevel);
+                worryLevel = Math.floor(worryLevel / 3);
+                console.log(
+                    "  Monkey gets bored with item. Worry level is divided by 3 to " +
+                        worryLevel
+                );
+                //console.log(item);
+                let newIndex = 0;
+                if (worryLevel % divisibleBy === 0) {
+                    //Throw to first
+                    //console.log("first");
+                    newIndex = Number(testTrue.split("monkey")[1]);
+                } else {
+                    //Throw to second
+                    //console.log("second");
+                    newIndex = Number(testFalse.split("monkey")[1]);
+                }
+
+                monkeyItems[newIndex].push(worryLevel);
+
+                console.log(
+                    "  Item with worry level " +
+                        worryLevel +
+                        " is thrown to monkey " +
+                        newIndex
+                );
+            });
+            monkeyItems.splice(monkeyIndex, 1, []);
         });
-        monekyItems.splice(monkeyIndex, 1, []);
+        console.log(monkeyItems);
+    }
+    //console.log(inspectedItems);
+    const inspectedItemsCount = inspectedItems.map((monkey) => {
+        return monkey.reduce((prevCount, count) => {
+            return prevCount + 1;
+        });
     });
-    console.log(inspectedItems);
-    console.log(monekyItems);
+    console.log(inspectedItemsCount);
 };
 run();
