0bd0b83e21118e191714bbc02143059c6387555f
olesien <olesien2@gmail.com>
2022-12-11 22:30:14 +0100

    bigint is not my friend

--- day-11.ts
+++ day-11.ts
@@ -3,16 +3,21 @@ const run = async () => {
     const monkeyList = monkeys.split("\n\n");
     console.log(monkeyList);
 
-    const monkeyItems = monkeyList.reduce((prevItems: number[][], monkey) => {
-        const [monkeyMisc, startingItems] = monkey.split("\n");
-        const items = startingItems.split(/[:,]+/).map((item) => Number(item));
-        prevItems.push(items);
-        return prevItems;
-    }, []);
-    const inspectedItems: number[][] = monkeyList.map(() => []);
+    const monkeyItems = monkeyList.reduce(
+        (prevItems: (bigint | number)[][], monkey) => {
+            const [monkeyMisc, startingItems] = monkey.split("\n");
+            const items = startingItems
+                .split(/[:,]+/)
+                .map((item) => Number(item));
+            prevItems.push(items);
+            return prevItems;
+        },
+        []
+    );
+    const inspectedItems: (bigint | number)[][] = monkeyList.map(() => []);
     //Add for loop here up to 20 for each round!
 
-    let counts: { [key: string]: number } = {
+    let counts: { [key: string]: bigint | number } = {
         "0": 0,
         "1": 0,
         "2": 0,
@@ -23,8 +28,8 @@ const run = async () => {
         "7": 0,
     };
 
-    for (let round = 1; round <= 20; round++) {
-        console.log("round " + round);
+    for (let round = 1; round <= 10000; round++) {
+        //console.log("round " + round);
         monkeyList.forEach((monkey, monkeyIndex) => {
             const [
                 monkeyMisc,
@@ -34,63 +39,63 @@ const run = async () => {
                 testTrue,
                 testFalse,
             ] = monkey.split("\n");
-            console.log(
-                monkeyMisc,
-                startingItems,
-                operation,
-                test,
-                testTrue,
-                testFalse
-            );
+            // console.log(
+            //     monkeyMisc,
+            //     startingItems,
+            //     operation,
+            //     test,
+            //     testTrue,
+            //     testFalse
+            // );
             const items = [...monkeyItems[monkeyIndex]];
             if (round === 1) {
                 items.shift();
             }
 
-            console.log(items);
+            //console.log(items);
 
             let worryLevel = 0;
             const operationStats = operation.split("=")[1];
             //console.log(operationStats);
-            const changeNumberLegacy: string | number =
+            const changeNumberLegacy: string | bigint | number =
                 operationStats.split(/[*+]+/)[1];
-            let changeNumber: string | number =
+            let changeNumber: string | bigint | number =
                 operationStats.split(/[*+]+/)[1];
-            const divisibleBy = Number(test.split("by")[1]);
+            const divisibleBy = BigInt(test.split("by")[1]);
 
             //Operation: First bit is ALWAYS "old", second can be "old" or number, with it either being * or + as seperator
-            const increaseWorryLevel = (item: number) => {
+            const increaseWorryLevel = (item: bigint | number) => {
                 if (String(changeNumberLegacy).includes("old")) {
                     changeNumber = item;
                 } else {
-                    changeNumber = Number(changeNumber);
+                    changeNumber = BigInt(changeNumber);
                 }
                 if (operationStats.includes("*")) {
                     //Mulitply
-                    return worryLevel * changeNumber;
+                    return BigInt(worryLevel) * BigInt(changeNumber);
                 } else {
                     //Plus
-                    return worryLevel + changeNumber;
+                    return BigInt(worryLevel) + BigInt(changeNumber);
                 }
             };
-            console.log("Monkey: " + monkeyIndex + ":");
+            //console.log("Monkey: " + monkeyIndex + ":");
 
             items.forEach((item, index) => {
-                console.log(
-                    " Monkey inspects an item with a worry level of " + item
-                );
+                // console.log(
+                //     " Monkey inspects an item with a worry level of " + item
+                // );
                 counts[String(monkeyIndex)] += 1;
                 worryLevel = item;
                 worryLevel = increaseWorryLevel(item);
-                console.log("  Worry level is changed to " + worryLevel);
-                worryLevel = Math.floor(worryLevel / 3);
-                console.log(
-                    "  Monkey gets bored with item. Worry level is divided by 3 to " +
-                        worryLevel
-                );
+                //console.log("  Worry level is changed to " + worryLevel);
+                //worryLevel = Math.floor(worryLevel / 3);
+                // console.log(
+                //     "  Monkey gets bored with item. Worry level is divided by 3 to " +
+                //         worryLevel
+                // );
                 //console.log(item);
-                let newIndex = 0;
-                if (worryLevel % divisibleBy === 0) {
+                let newIndex: number = 0;
+                if (BigInt(worryLevel) % BigInt(divisibleBy) === BigInt(0)) {
                     //Throw to first
                     //console.log("first");
                     newIndex = Number(testTrue.split("monkey")[1]);
@@ -102,16 +107,16 @@ const run = async () => {
 
                 monkeyItems[newIndex].push(worryLevel);
 
-                console.log(
-                    "  Item with worry level " +
-                        worryLevel +
-                        " is thrown to monkey " +
-                        newIndex
-                );
+                // console.log(
+                //     "  Item with worry level " +
+                //         worryLevel +
+                //         " is thrown to monkey " +
+                //         newIndex
+                // );
             });
             monkeyItems.splice(monkeyIndex, 1, []);
         });
-        console.log(monkeyItems);
+        //console.log(monkeyItems);
     }
     //console.log(inspectedItems);
     // const inspectedItemsCount = inspectedItems.map((monkey) => {
